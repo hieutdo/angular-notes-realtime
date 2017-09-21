60dcd137641cb8978ea9fbc9011caabb4d96baef
Hieu Do <mr.hieudt@gmail.com>
2017-09-20 20:30:37 -0500

    Enable redux-devtools when environment is not production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
 import { SocketService } from './core/services/socket.service';
@@ -9,7 +11,12 @@ import * as fromRoot from './core/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutes, StoreModule.forRoot(fromRoot.reducers)],
+  imports: [
+    BrowserModule,
+    AppRoutes,
+    StoreModule.forRoot(fromRoot.reducers),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   providers: [SocketService],
   bootstrap: [AppComponent],
 })
